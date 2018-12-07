c41f009ad6da76d4153a4c22e6743f47c01105d7
kwintynmporter <kwintynmporter@lewisu.edu>
2018-12-06 23:04:50 -0800

    moved dragon attack

--- dragonjav.js
+++
@@ -1,4 +0,0 @@
-$('#video').on('click', function() {
-    $(this).html('<iframe src="https://www.youtube.com/embed/a6KGPBflhiM?rel=0&autoplay=1" width="320" height="240" frameborder="0" allowfullscreen="false">')
-    .addClass('active');
-  });
